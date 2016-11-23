63cbe6ac357dcf3e874e319ce488c72c45e63e07
3846masa <3846masahiro+git@gmail.com>
2016-11-23 15:57:44 +0900

    Update commit script

--- commit.sh
+++ commit.sh
@@ -6,5 +6,6 @@ git commit $@ &&
 git symbolic-ref HEAD refs/heads/build &&
 git reset && git checkout .gitignore &&
 npm run build &&
-git commit --allow-empty -am 'build '$(git log develop --pretty=format:'%H' -n 1) &&
+git add -A &&
+git commit --allow-empty -m 'build '$(git log develop --pretty=format:'%H' -n 1) &&
 git checkout $CURRENT_BRANCH
