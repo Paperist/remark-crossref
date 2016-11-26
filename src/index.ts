2f93de58b7b683e6c26b25d2ca1c1c338c02bad6
3846masa <3846masahiro+git@gmail.com>
2016-11-26 19:01:53 +0900

    Remove TableCaption

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,4 @@
 /**
- * Static
- */
-const C_NEWLINE = '\n';
-const C_SPACE = '\x20';
-const C_TAB = '\t';
-
-/**
- * Tokenise a cross-ref label.
- *
- * @example
- *   tokenizeTableCaption(eat, 'Table: Caption {#tbl:table}');
- */
-function tokenizeTableCaption (
-  this: any,
-  eat: any,
-  value: string,
-  silent: boolean = false,
-) {
-  const length = value.length;
-  let firstLine = '';
-  let idx = 0;
-
-  for ( ; idx < length; idx++) {
-    const char = value.charAt(idx);
-    firstLine += char;
-    if (char === C_NEWLINE) {
-      break;
-    }
-  }
-
-  for (idx++; idx < length; idx++) {
-    const char = value.charAt(idx);
-    if (char === C_SPACE || char === C_TAB) {
-      continue;
-    }
-    if (char === C_NEWLINE) {
-      break;
-    }
-    return;
-  }
-
-  const isMatched = new RegExp(/^(?:\s*table)?\s*:.*$/, 'im').test(firstLine);
-  if (!isMatched) {
-    return;
-  }
-
-  if (silent) {
-    return true;
-  }
-
-  return eat(firstLine)({
-    children: this.tokenizeInline(firstLine, eat.now()),
-    type: 'tableCaption',
-  });
-}
-
-/**
  * Tokenise a cross-ref label.
  *
  * @example
@@ -154,16 +97,11 @@ function locateCrossReference (
  */
 function attacher(remark: any) {
   const proto = remark.Parser.prototype;
-  const blockMethods = proto.blockMethods;
   const inlineMethods = proto.inlineMethods;
 
   /**
    * Add a tokenizer to the `Parser`.
    */
-  proto.blockTokenizers.tableCaption =
-    Object.assign(tokenizeTableCaption, { locator: locateCrossReferenceLabel });
-  blockMethods.splice(inlineMethods.indexOf('paragraph'), 0, 'tableCaption');
-
   proto.inlineTokenizers.crossReferenceLabel =
     Object.assign(tokenizeCrossReferenceLabel, { locator: locateCrossReferenceLabel });
   inlineMethods.splice(inlineMethods.indexOf('text'), 0, 'crossReferenceLabel');
